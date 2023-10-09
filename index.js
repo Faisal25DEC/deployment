2cf6cebc3b7fc339722a1360fa42016ee8d7eb62
Faisal25DEC <faisalmd25121999@gmail.com>
2023-10-09 19:57:46 +0530

    comit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("server started");
